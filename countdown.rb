9747f38ee32073265c8aed0854911f66e603bda2
Abraham Moreno <adiaz2821@gmail.com>
2019-10-09 13:49:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@ while countdown > 0
 puts "#{countdown} SECOND(S)!"
 countdown -= 1
 puts "HAPPY NEW YEAR!"
-countdown_with_sleep = sleep()
+countdown_with_sleep = sleep(1.second)
 def countdown
   
 end
